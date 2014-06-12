486f753d306e74fdb9a189b2806ab1290d2c9c5a
D*isuke YAMAKAWA <dicek.am@clockahead.com>
2014-06-12 19:10:02 +0900

    first release

---
+++ js/neuecc.js
@@ -0,0 +1,5 @@
+﻿$('body').css('background', 'white');
+$('*').not('.csharp >span').css('color', 'black');
+$('#s').css('background', 'white');
+$('#comment').css('background', 'white');
+$('input').css('background', 'white'); /*for "Search" button has no id */
